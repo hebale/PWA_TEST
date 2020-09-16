61074132e5372e79eeaaef98ad03c9a75bac060e
hebale <hebale@eilab.kr>
2020-09-16 18:18:38 +0900

    test reset

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,7 @@
 (function () {
   'use strict';
 
-  let appServerPublicKey = 'BB2r4EpojQZolyzE_PVUssCT8NhOCqcYjJfbW4VLRzEac7F9-sY_uOFqn9MQI6rS6JmLwGfMZxmBcxoRed7sazE';
-  let isSubscribed = false; 
-  let swRegist = null;
-
-  function urlB64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-      .replace(/\-/g, '+')
-      .replace(/_/g, '/');
-  
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-  
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-  }
-
-  const app = {
+  var app = {
     images: [],
     imageArea: document.getElementById('image-area')
   };
@@ -31,8 +12,8 @@
   app.showImage = function () {
     this.imageArea.innerHTML = '';
     this.images.forEach(image => {
-      let imageDiv = document.createElement('div');
-      let img = document.createElement('img');
+      var imageDiv = document.createElement('div');
+      var img = document.createElement('img');
       img.src = image;
       imageDiv.classList.add('image');
       imageDiv.appendChild(img);
@@ -77,106 +58,10 @@
     }
   }
 
-  /*========== TODO: 아래에 Push 관련 로직 구현 ========== */
-  // 구독 버튼 상태 갱신
-  function updateButton () {
-    // TODO: 알림 권한 거부 처리
-
-    const pushButton = document.getElementById('subscribe')
-    if (isSubscribed) {
-      pushButton.textContent = 'Disable Push Messaging';
-    } else {
-      pushButton.textContent = 'Enable Push Messaging';
-    }
-    pushButton.disabled = false;
-  }
-
-  // 구독 정보 갱신
-  function updateSubscription (subscription) {
-    // TODO: 구독 정보 서버로 전송
-
-    let detailArea = document.getElementById('subscription_detail')
-
-    if (subscription) {
-      detailArea.innerText = JSON.stringify(subscription)
-      detailArea.parentElement.classList.remove('hide')
-    } else {
-      detailArea.parentElement.classList.add('hide')
-    }
-  }
-
-  // 알림 구독
-  function subscribe () {
-    const applicationServerKey = urlB64ToUint8Array(appServerPublicKey);
-    swRegist.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: applicationServerKey
-    })
-    .then(subscription => {
-      console.log('User is subscribed.');
-      updateSubscription(subscription);
-      isSubscribed = true;
-      updateButton();
-    })
-    .catch(err => {
-      console.log('Failed to subscribe the user: ', err);
-      updateButton();
-    });
-  }
-
-  // 알림 구독 취소
-  function unsubscribe () {
-
-  }
-
-  // Push 초기화
-  function initPush () {
-    const pushButton = document.getElementById('subscribe')
-    pushButton.addEventListener('click', () => {
-      if (isSubscribed) {
-        // TODO: 구독 취소 처리
-      } else {
-        subscribe();
-      }
-    });
-
-    swRegist.pushManager.getSubscription()
-      .then(function(subscription) {
-        isSubscribed = !(subscription === null);
-        updateSubscription(subscription);
-
-        if (isSubscribed) {
-          console.log('User IS subscribed.');
-        } else {
-          console.log('User is NOT subscribed.');
-        }
-
-        updateButton();
-      });
-  }
-
-
   // TODO: 아래에 서비스워커 등록
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js').then(regist => {
-      swRegist = regist;
+    navigator.serviceWorker.register('./service-worker.js').then(() => {
       console.log('Service Worker Registered');
-
-      // TODO: Push 기능 초기화
-      initPush();
-
-      regist.addEventListener('updatefound', () => {
-        const newWorker = regist.installing;
-        console.log('Service Worker update found!');
-
-        newWorker.addEventListener('statechange', function () {
-          console.log('Service Worker state changed:', this.state);
-        });
-      });
-    });
-
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      console.log('Controller changed');
     });
   }
 
