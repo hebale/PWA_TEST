7552eba8230a03f2c3fb797c7a01cf5f57847973
hebale <hebale@eilab.kr>
2020-09-17 14:33:29 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,49 +5,61 @@
 const _version = 'v4';
 const cacheName = 'cacheBox2';
 const cacheList = [
-  './',
-  './manifest.json',
-  './images/1.png',
-  './images/2.png',
-  './images/3.png',
-  './images/4.png',
-  './images/5.png',
-  './styles/index.css',
-  './scripts/app.js'
+	'./',
+	'./manifest.json',
+	'./images/1.png',
+	'./images/2.png',
+	'./images/3.png',
+	'./images/4.png',
+	'./images/5.png',
+	'./styles/index.css',
+	'./scripts/app.js'
 ]
 
 const log = msg => {
-  console.log(`[ServiceWorker ${_version}] ${msg}`);
+	console.log(`[ServiceWorker ${_version}] ${msg}`);
 }
 
 // Life cycle: INSTALL
 self.addEventListener('install', event => {
 
-  self.skipWaiting();
-  log('INSTALL');
+	self.skipWaiting();
+	log('INSTALL');
 
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache){
-      log('Caching app shell');
-      return cache.addAll(cacheList);      
-    })
-  );
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			log('Caching app shell');
+			return cache.addAll(cacheList);      
+		})
+	);
 
 });
 
 // Life cycle: ACTIVATE
 self.addEventListener('activate', event => {
-  log('Activate');
+	log('Activate');
+
+	event.waitUntil(
+		cache.keys().then(keyList => {
+			return Promise.all(keyList.map(key => {
+                if(key !== cacheName){
+                    log('.Removing old cache ' + key);
+                    return caches.delete(key);
+                }
+            }));
+		})
+	);
+
 });
 
 // Functional: FETCH
 self.addEventListener('fetch', event => {
-  log('Fetch ' + event.request.url);
+	log('Fetch ' + event.request.url);
 
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			return response || fetch(event.request);
+		})
+	);
 
 });
